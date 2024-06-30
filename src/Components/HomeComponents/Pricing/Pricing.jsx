71dcd2d799dd83e40e3cba63cbac84b296fc22db
Robiu-Sani <hafazrobiussani@gmail.com>
2024-06-30 22:16:03 +0600

    setup light them is done

--- src/Components/HomeComponents/Pricing/Pricing.jsx
+++ src/Components/HomeComponents/Pricing/Pricing.jsx
@@ -12,6 +12,17 @@ export default function Pricing() {
         name={`Flexible pricing for everyone`}
         title={`Explore the various subscription options.`}
       />
+      <div className="w-full flex justify-center items-center pt-10">
+        <label className="flex cursor-pointer mt-5 gap-2">
+          <span className="label-text">Monthly</span>
+          <input
+            type="checkbox"
+            value="synthwave"
+            className="toggle theme-controller"
+          />
+          <span className="label-text">Yearly</span>
+        </label>
+      </div>
       <div className="max-w-[1600px] mx-auto flex justify-center items-start flex-col gap-5 px-2 2xl:gap-10 2xl:flex-row">
         <StarterPlan></StarterPlan>
         <ProPlan></ProPlan>
